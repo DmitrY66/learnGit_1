a79cb3df41bd13735a91d6ac5f10c7b13e251fe9
Dmitri Gorun <iii448@mail.ru>
2021-11-19 22:35:08 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const nameGood = 'Куртка';
+
+const quantityGood = 6;
+
+const categoryGoods = 'Верхняя одежда';
+
+const priceGood = 6000;
+
+
+console.log('наименование товара: ', nameGood);
+
+console.log('общая сумма товара: ', +quantityGood * +priceGood);
+
