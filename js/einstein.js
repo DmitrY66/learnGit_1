61b4066c6aa20b0372f9f3149eb3f221b33c97df
Dmitri Gorun <iii448@mail.ru>
2021-11-23 00:12:43 +0500

    lesson3

---
+++ js/einstein.js
@@ -0,0 +1,7 @@
+const catWeight = 88;
+
+const speedLight = 3e8;
+
+const E = catWeight * speedLight ** 2;
+
+console.log('энергия тела моего кота =', E, 'J');
--- js/main.js
+++ js/main.js
@@ -9,9 +9,9 @@ const categoryGoods = 'Верхняя одежда';
 
 const priceGood = 6000;
 
-console.log('наименование товара: ', nameGood);
+// console.log('наименование товара: ', nameGood);
 
-console.log('общая сумма товара: ', +quantityGood * +priceGood);
+// console.log('общая сумма товара: ', +quantityGood * +priceGood);
 
 }
 
@@ -24,8 +24,35 @@ const categoryGoods = 'Космическая одежда';
 
 const priceGood = 66000;
 
-console.log('наименование товара: ', nameGood);
+// console.log('наименование товара: ', nameGood);
 
-console.log('общая сумма товара: ', +quantityGood * +priceGood);
+// console.log('общая сумма товара: ', +quantityGood * +priceGood);
+
+}
+
+{
+const nameGood = 'Гравицапа';
+
+const quantityGood = 26;
+
+const categoryGoods = 'Запчасти для звездолёта';
+
+const priceGood = 2e6;
+
+const getNameGood = prompt('введите наименование товара', 'Гравицапа');
+const getQuantityGood = +prompt('введите количество товара', 26);
+const getCategoryGoods = prompt('введите категорию товара', 'Запчасти для звездолёта');
+const getPriceGood = +prompt('введите цену товара', 2e6);
+
+console.log('наименование товара: ', getNameGood, '|| тип', typeof getNameGood);
+console.log('количество товара: ', getQuantityGood, '|| тип', typeof getQuantityGood);
+console.log('категория товара: ', getCategoryGoods, '|| тип', typeof getCategoryGoods);
+console.log('цена товара: ', getPriceGood, '|| тип', typeof getPriceGood);
+
+console.log(`На складе:  ${getQuantityGood} единиц товара "${getNameGood}" на общую сумму ${getQuantityGood * getPriceGood} КэЦэ`);
+
+// console.log('наименование товара: ', nameGood);
+
+// console.log('общая сумма товара: ', +quantityGood * +priceGood);
 
 }
